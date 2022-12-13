566715cdcf8e98bde4dd6a0312cb67edf06d7778
Eddie <aditya.subedi@gmail.com>
2022-12-12 17:34:08 -0800

    Added previous week functionality

--- src/components/MobileCalendar.svelte
+++ src/components/MobileCalendar.svelte
@@ -2,6 +2,7 @@
   import { swipe } from 'svelte-gestures';
   import {startDate} from "../store/stores.js";
   import {Calendar} from "./calendar.js";
+  import { v4 as uuidv4 } from 'uuid';
 
   var calendar = Calendar(startDate);
 
@@ -9,6 +10,7 @@
   $: month = calendar.getMonth()
   $: year = calendar.getYear()
   $: translateX = 0
+  $: layout = [{ id: uuidv4() },{ id: uuidv4() },{ id: uuidv4() }]
 
   function next() {
     // weekCalendar = calendar.getNextWeekCalendar();
@@ -36,33 +38,31 @@
     var {direction} = detail;
     console.log(direction)
     if(direction=='left') {
-      translateX = -50
+      // layout = [...layout.slice(1,layout.length)]
+      // layout = [...layout, { id: uuidv4() }]
+      translateX = translateX - (100/layout.length)
+      console.log(layout)
     } else {
-      translateX = 0
+      // numbers = [...numbers, numbers.length + 1]
+      translateX = translateX + (100/layout.length)
     }
+
   }
 </script>
 
 <h2 class="text-center">{month.long}, {year}</h2>
-<div class="flex w-[200%] -mx-3 select-none transition-transform" style="transform:translateX({translateX}%)">
-  <div class="flex justify-between mt-2 w-full"
-       use:swipe={{ timeframe: 1000, minSwipeDistance: 60 }} on:swipe={handler}>
-    {#each weekCalendar as week}
-      <div class="{week.isToday ? 'font-bold bg-[#F5F6FA]': ''} flex flex-col items-center py-5 px-3 rounded-lg text-[#283140]">
-        <p>{week.day}</p>
-        <p>{week.weekday}</p>
-      </div>
-    {/each}
-  </div>
-  <div class="flex justify-between mt-2 w-full"
-       use:swipe={{ timeframe: 1000, minSwipeDistance: 60 }} on:swipe={handler}>
-    {#each weekCalendar as week}
-      <div class="{week.isToday ? 'font-bold bg-red-500': ''} flex flex-col items-center py-5 px-3 rounded-lg text-[#283140]">
-        <p>{week.day}</p>
-        <p>{week.weekday}</p>
-      </div>
-    {/each}
-  </div>
+<div class="flex w-[{layout.length*100}%] -mx-3 select-none transition-transform" style="transform:translateX({translateX}%)">
+  {#each layout as l (l.id)}
+    <div class="flex justify-between mt-2 w-full px-4"
+         use:swipe={{ timeframe: 1000, minSwipeDistance: 60 }} on:swipe={handler}>
+      {#each weekCalendar as week}
+        <div class="{week.isToday ? 'font-bold bg-[#F5F6FA]': ''} flex flex-col items-center py-5 px-3 rounded-lg text-[#283140]">
+          <p>{week.day}</p>
+          <p>{week.weekday}</p>
+        </div>
+      {/each}
+    </div>
+  {/each}
 </div>
 <button class="mt-10 border-4 px-5 py-2 bg-[#DFECF5] border-transparent rounded-lg" on:click={prev}>Prev</button>
 <button class="mt-10 border-4 px-5 py-2 bg-[#EDE5F2] border-transparent rounded-lg ml-3" on:click={next}>Next</button>
